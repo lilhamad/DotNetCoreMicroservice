447f5c846704cf00a718caaf7dabd98c5c1c743e
Adewale Abdulhammed <lilhamad8@hotmail.com>
2020-05-06 15:05:46 +0100

    added user class

--- Actio.Services.Identity/Domain/Models/User.cs
+++ Actio.Services.Identity/Domain/Models/User.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+namespace IdentityService_Microservice.Actio.Services.Identity.Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string email, string name)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentNullException("Empty email");
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Empty name");
+            }
+            Id = Guid.NewGuid();
+            Email = email.ToLowerInvariant();
+            CreatedAt = DateTime.UtcNow;
+
+        }
+    }
+}
