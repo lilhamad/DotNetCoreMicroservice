e724636819b9af349aa70d7b29c930448d7369ec
Adewale Abdulhammed <lilhamad8@hotmail.com>
2020-05-06 15:09:57 +0100

    empty iuserrepository

--- Actio.Services.Identity/Domain/Repositories/IUserRepository.cs
+++ Actio.Services.Identity/Domain/Repositories/IUserRepository.cs
@@ -1,15 +1,1 @@
-﻿using IdentityService_Microservice.Actio.Services.Identity.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace IdentityService_Microservice.Actio.Services.Identity.Domain.Repositories
-{
-    interface IUserRepository
-    {
-        Task<User> GetAsync(Guid Id);
-        Task<User> GetAsync(string email);
-        Task AddAsync(User user);
-    }
-}
+﻿
